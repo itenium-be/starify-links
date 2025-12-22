4014564abf4d2745574794d6bac6e647c16ab28f
Wouter Van Schandevijl <woutervs@hotmail.com>
2025-12-22 20:02:46 +0100

    options: display current version tag

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,7 +1,16 @@
 import { setupBadgesOptions } from './badgesOptions';
 import { setupDirectActivationOptions } from './directActivationOptions';
 
+function displayVersion() {
+  const manifest = chrome.runtime.getManifest();
+  const versionEl = document.getElementById('versionInfo');
+  if (versionEl) {
+    versionEl.textContent = `v${manifest.version}`;
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   setupBadgesOptions();
   setupDirectActivationOptions();
+  displayVersion();
 });
